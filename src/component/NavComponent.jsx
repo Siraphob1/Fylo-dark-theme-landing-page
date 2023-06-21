a90153571f3dd4d5e741dfcfeaf71d299eb2e7b2
Siraphob1 <Sirapop_t@hotmail.com>
2023-06-21 15:13:01 +0700

    import logo & flex nav

--- src/component/NavComponent.jsx
+++ src/component/NavComponent.jsx
@@ -1,11 +1,17 @@
-
+import logo from "../images/logo.svg";
 
 
 const NavComponent = () => {
   return (
-    <div>
-      
-    </div>
+    <nav className="bgc-intro-email flex  justify-between items-center px-[1rem] py-[2rem]
+                    xl:px-[4rem] "> 
+        <img src={logo} alt="logo" className="w-[90px]"/>
+        <ul className="flex">
+            <li><a href="#" className="nav-a">Features</a></li>
+            <li><a href="#" className="nav-a">Team</a></li>
+            <li><a href="#" className="nav-a">Sign In</a></li>
+        </ul>
+    </nav>
   )
 }
 
