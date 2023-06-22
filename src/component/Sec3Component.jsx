7b42626210815c90e608193078ae4de7f7e53541
Siraphob1 <Sirapop_t@hotmail.com>
2023-06-22 16:01:49 +0700

    add validate email

--- src/component/Sec3Component.jsx
+++ src/component/Sec3Component.jsx
@@ -1,3 +1,4 @@
+import { useEffect, useState } from "react";
 import quote from "../images/bg-quotes.png";
 import propfile1 from "../images/profile-1.jpg";
 import propfile2 from "../images/profile-2.jpg";
@@ -6,6 +7,13 @@ import propfile3 from "../images/profile-3.jpg";
 
 
 const Sec3Component = () => {
+  const [email,setEmail] = useState('');
+  const [loginsuccess,setLoginsuccess] = useState(true);
+
+  useEffect(()=>{
+    alert(`should login by [hotmail.com /gmail.com /outlook.com /outlook.co.th]`)
+  },[])
+  
   const data = [
     {
       paragraph:'Fylo has improved our team productivity by an order of magnitude. Since making the switch our team has      become a well-oiled collaboration machine.',
@@ -27,6 +35,31 @@ const Sec3Component = () => {
     }
   ]
 
+  const Validate=(event)=>{
+    event.preventDefault()
+      const emailtype =["hotmail.com" , "gmail.com" , "outlook.com" , "outlook.co.th"]
+      let mailformat = /[A-Za-z]+@/
+
+      if(email.match(mailformat)){
+        let emailinput =String(email)
+        let indexat = emailinput.indexOf("@");
+        let wordend = emailinput.substring(indexat+1)
+        
+        if(wordend === emailtype[0]||wordend === emailtype[1] ||wordend === emailtype[2] ||wordend === emailtype[3]){
+          setLoginsuccess(true);
+          setEmail('');
+          return
+        }
+        
+      }
+    
+    setLoginsuccess(false);
+    console.log(`should login by[${emailtype[0]} ,${emailtype[1]} ,${emailtype[2]} ,${emailtype[3]}]`)
+  }
+  const ChangeInput =(e)=>{
+    setEmail(e.target.value);
+  }
+
   return (
     <section className="pt-[4rem] pb-[20rem] px-[2rem]  relative
                         xl:px-[6rem]">
@@ -37,7 +70,7 @@ const Sec3Component = () => {
                       <p className=" font-fontOpenSans text-[11px] xl:text-[1rem]">{element.paragraph}</p>
                       <div className="mt-[2rem] flex items-center">
                         <img src={element.profile} alt={element.profile} className="profile-sec3"/>
-                        <div className="flex flex-col xl:ml-[1rem]">
+                        <div className="flex flex-col ml-[1rem]">
                           <span className=" font-fontOpenSans font-bold text-[10px] xl:text-[14px]">{element.name}</span>
                           <span className=" font-fontRaleway text-[8px] xl:text-[10px]">{element.position}</span>
                         </div>
@@ -50,10 +83,20 @@ const Sec3Component = () => {
           <p className="cardb-p-sec3"> It only takes a minute to sign up and our free starter tier is extremely generous. If you have any 
               questions, our support team would be happy to help you.
           </p>
-          <div className="flex flex-col items-center xl:flex-row xl:justify-center">
+          {/* <div className="flex flex-col items-center xl:flex-row xl:justify-center">
               <input type="text" placeholder="email@example.com" className="cardb-input-sec3"/>
               <button className="btn-header px-[4rem] py-[0.8rem] w-[280px] xl:w-auto">Get Started For Free</button>
-          </div>
+          </div> */}
+          <form onSubmit={Validate} className="flex flex-col items-center xl:flex-row xl:justify-center">
+            <div className=" relative">
+              <input type="text" placeholder="email@example.com" className="cardb-input-sec3 " value={email} onChange={ChangeInput} />
+              <div className={`${loginsuccess   ?'hidden '
+                                                :'suggest-sec3'}`}>
+                Please enter a valid email address
+              </div>
+            </div>
+            <button type="submit" className="btn-header px-[4rem] py-[0.8rem] w-[280px] xl:w-auto">Get Started For Free</button>
+          </form>
       </section>
     </section>
   )
